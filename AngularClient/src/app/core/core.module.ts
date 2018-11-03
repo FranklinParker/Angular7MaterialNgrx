a627429a1fd63679bf244523353c23c2035364b6
Franklin Parker <fparker@deliveryman.com>
2018-11-03 00:54:49 -0400

    home page

--- AngularClient/src/app/core/core.module.ts
+++ AngularClient/src/app/core/core.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavHeaderComponent } from './components/nav-header/nav-header.component';
 import {SharedModule} from '../shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    NavHeaderComponent
+    NavHeaderComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    NavHeaderComponent
+    NavHeaderComponent,
+    HomeComponent
   ]
 })
 export class CoreModule { }
