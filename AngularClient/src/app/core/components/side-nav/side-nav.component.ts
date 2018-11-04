92e973207a7d4dcdc814097bb76c27d482c5e871
Franklin Parker <fparker@deliveryman.com>
2018-11-04 08:47:25 -0500

    side nav close

--- AngularClient/src/app/core/components/side-nav/side-nav.component.ts
+++ AngularClient/src/app/core/components/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-
+   @Output() closeSidNav = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
