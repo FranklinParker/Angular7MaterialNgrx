9b8964542ae24b63f5bf50706b8bf82d0f42bd2a
Franklin Parker <fparker@deliveryman.com>
2018-11-04 22:15:50 -0500

    gen a store

--- AngularClient/src/app/app.module.ts
+++ AngularClient/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 import {SharedModule} from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {EffectsModule} from '@ngrx/effects';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,11 @@ import {SharedModule} from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+
   ],
   providers: [],
   bootstrap: [AppComponent]
