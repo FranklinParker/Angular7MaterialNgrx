c85cf40f4132fa0df7c62491608256d4f33ec7fe
Franklin Parker <fparker@deliveryman.com>
2018-11-08 20:59:35 -0500

    table

--- AngularClient/src/app/contact/components/contact-list/contact-list.component.ts
+++ AngularClient/src/app/contact/components/contact-list/contact-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {AppState} from '../../../reducers';
+import {getAllContacts} from '../../store/contact.selector';
+import {map} from 'rxjs/operators';
+import {Contact} from '../../models/contact';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-list.component.scss']
 })
 export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [];
+  isLoading: boolean = false;
 
-  constructor() { }
+  selectedId: number;
+  totalRecords = 10;
+  recordsPerPage = 5;
+  recordUpdating = false;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 15];
 
-  ngOnInit() {
+  dataSource =  new MatTableDataSource<Contact>([]);
+
+  displayedColumns = ['id', 'firstName', 'lastName','delete'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private store: Store<AppState>) {
   }
 
+  ngOnInit() {
+    this.store
+      .pipe(
+        select(getAllContacts)
+      )
+      .subscribe((contacts: Contact[]) => {
+        this.contacts = contacts;
+        console.log('contacts', this.contacts);
+        this.dataSource.data = this.contacts;
+        console.log('data source len:' + this.dataSource.data.length)
+      });
+  }
+
+  onDelete( contact: Contact) {
+
+  }
+
+  get dataLoaded(){
+    return this.dataSource.data && this.dataSource.data.length>0;
+  }
+  get showSpinner(){
+    return this.isLoading;
+  }
 }
